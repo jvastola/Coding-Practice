662ee08245aa6abc3a27483a6dd2cd5e2a33f5de
Johnny Vastola <32178472+jvastola@users.noreply.github.com>
2020-06-01 15:19:44 -0700

    Create adjacentElementsProduct.cpp

---
+++ adjacentElementsProduct.cpp
@@ -0,0 +1,8 @@
+int adjacentElementsProduct(std::vector<int> inputArray) {
+    int max=inputArray[0]*inputArray[1];
+     for(int i=0;i<inputArray.size()-1;i++){
+         if(max<inputArray[i]*inputArray[i+1])
+         max=inputArray[i]*inputArray[i+1];
+     }
+     return max;
+}
