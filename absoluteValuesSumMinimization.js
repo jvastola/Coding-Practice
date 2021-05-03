6e153964087f93e2256dc5068859d657b14432d0
Johnathan Vastola <32178472+jvastola@users.noreply.github.com>
2021-05-02 17:14:22 -0700

    Create absoluteValuesSumMinimization.js

---
+++ absoluteValuesSumMinimization.js
@@ -0,0 +1,16 @@
+function absoluteValuesSumMinimization(a) {
+    let result = a[0];
+    let total = Number.MAX_VALUE;
+
+    for(let i=0; i<a.length; i++){
+        let tmp = 0;
+        for(let j=0; j<a.length; j++){
+            tmp += Math.abs(a[j]-a[i]);
+        }
+        if(tmp < total) {
+            result = a[i];
+            total = tmp;
+        }
+    }
+    return result;
+}
